5b7e428ddb1e71727bf24cbd856c8f07f8d8c5c7
berkkiyi <kyberk1234@hotmail.com>
2025-10-04 00:38:44 +0300

    Accommodate Keycloak ID into UserService

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -17,7 +17,18 @@ public class UserService {
     public UserResponse register(RegisterRequest request) {
 
         if(repository.existsByEmail(request.getEmail())){
-            throw new RuntimeException("Email already exist");
+
+            User existingUser = repository.findByEmail(request.getEmail() );
+            UserResponse userResponse = new UserResponse();
+            userResponse.setId(existingUser.getId());
+            userResponse.setKeycloakId(existingUser.getKeycloakId());
+            userResponse.setPassword(existingUser.getPassword());
+            userResponse.setEmail(existingUser.getEmail());
+            userResponse.setFirstName(existingUser.getFirstName());
+            userResponse.setLastName(existingUser.getLastName());
+            userResponse.setCreatedAt(existingUser.getCreatedAt());
+            userResponse.setUpdatedAt(existingUser.getUpdatedAt());
+            return userResponse;
         }
         User user = new User();
         user.setEmail(request.getEmail());
@@ -28,6 +39,7 @@ public class UserService {
         User savedUser = repository.save(user);
         UserResponse userResponse = new UserResponse();
         userResponse.setId(savedUser.getId());
+        userResponse.setKeycloakId(savedUser.getKeycloakId());
         userResponse.setPassword(savedUser.getPassword());
         userResponse.setEmail(savedUser.getEmail());
         userResponse.setFirstName(savedUser.getFirstName());
@@ -55,6 +67,6 @@ public class UserService {
 
     public Boolean existsByUserId(String userId) {
         log.info("Calling User Validation API for userId: {}",userId);
-        return repository.existsById(userId);
+        return repository.existsByKeycloakId(userId);
     }
 }
